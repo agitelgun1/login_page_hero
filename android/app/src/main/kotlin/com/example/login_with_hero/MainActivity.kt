53bef8ad9d71aae3ddf4ee7f3f8fa80e72d8eb7e
agitelgun <agit@talktoivy.com>
2020-09-22 14:13:40 +0300

    App commit

---
+++ android/app/src/main/kotlin/com/example/login_with_hero/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.login_with_hero
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:login_with_hero/login_page.dart';
+import 'package:login_with_hero/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.lightBlue, fontFamily: 'Nunito'),
+      home: LoginPage(),
+      routes: routes,
+    );
+  }
+}
